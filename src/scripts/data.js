333697576d48628c09f1d152f3469d7018528d58
tharon-c <design@tharoncarlson.com>
2017-08-30 11:19:26 -0700

    clean up example

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,13 +1,16 @@
 const R = require('ramda');
 
 const fillDummy = i => {
-    const value = (i.type == "checkbox")
-        ? true : "It Works!";
+    // Most feilds are actually radio buttons which are of type
+    // checkbox. In this case we would have to specify the
+    // name of the field for value. Setting to false at least
+    // shows that the checkbox is affected as every radio set
+    // is on last option.
+    const value = ( i.type === "checkbox" )
+        ? true : `${i.name} works!`;
     return { [i.name]: value }
 };
 
-exports.genData = function(list) {
-    return (
-        R.mergeAll(list.map(fillDummy))
-    )
-}
+exports.genData = list => (
+    R.mergeAll( list.map( fillDummy ) )
+);
--- src/scripts/makePDF.js
+++ src/scripts/makePDF.js
@@ -3,31 +3,30 @@ const path = require('path');
 const fs = require('fs');
 const { genData } = require("./data");
 
-const targetFile = path.resolve(__dirname, 'F245-127-000.pdf');
+const templateFile = path.resolve(__dirname, 'F245-127-000.pdf');
 const outputFile = "test123.pdf";
 
 const writeFile = templateFields => {
     pdfFillForm.write(
-        targetFile,
-        genData(templateFields),
+        templateFile,
+        genData( templateFields ),
         { "save": "pdf" }
     )
     .then( result => {
         fs.writeFile( outputFile, result, err => {
             if(err) {
-                return console.log(err);
+                return console.log( err );
             }
-            console.log("The file was saved!");
+            console.log( "The file was saved!" );
         }); 
     }, err => {
-        console.log(err);
+        console.log( err );
     });
 };
 
-pdfFillForm.read(targetFile)
+pdfFillForm.read(templateFile)
     .then( result => {
-        console.log(result);
         writeFile( result )
     }, err => {
-        console.log(err);
+        console.log( err );
     });
